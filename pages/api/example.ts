0d44bbbe14cffd0de3e53ecc51d07b43f95a8dfa
Steven Du <sdu1278@gmail.com>
2022-12-28 15:45:44 -0500

    Fix Linter

--- pages/api/example.ts
+++ pages/api/example.ts
@@ -2,17 +2,17 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { clientPromise, client } from '../../lib/mongodb';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const isConnected = await clientPromise;
-    if (isConnected) {
-      const db = client.db('facemash');
-      const profile = db.collection('profile');
-      console.log(profile);
-      res.json(profile);
-    }
-  } catch (e) {
-    console.error(e);
-  }
+	try {
+		const isConnected = await clientPromise;
+		if (isConnected) {
+			const db = client.db('facemash');
+			const profile = db.collection('profile');
+			console.log(profile);
+			res.json(profile);
+		}
+	} catch (e) {
+		console.error(e);
+	}
 };
 
 export default handler;
