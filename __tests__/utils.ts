1dd81f2331d2a291c71627cce26bccd8472be29a
Jackie Mok <96596043+jmok3634@users.noreply.github.com>
2023-05-17 15:34:53 -0400

    Revert "create test suite, move dependencies to dev dependencies (#10)" (#12)
    This reverts commit 5908a47d6d7b38dffc698a36ef42fbac0e54e50e.

--- __tests__/utils.ts
+++
@@ -1,7 +0,0 @@
-import { render } from '@testing-library/react';
-import { ReactElement } from 'react';
-
-const customRender = (ui: ReactElement, options?: Record<string, unknown>) => render(ui, { ...options });
-
-export * from '@testing-library/react';
-export { customRender as render };
